e96ca6795fe6812f4d4afefeca90d35c572e8493
bjarnerossen <92723881+bjarnerossen@users.noreply.github.com>
2024-09-15 20:55:55 -0700

    StartCoordinator
    På dessa tre sidor ska du istället för den hårdkodade datat som finns, hämta data från den falska databasen. Datat hämtas och används för att fylla på i tabellen med ärenden och i de drop-downlistor som finns på respektive sida.
    
    Ärendenumret ska fortsatt vara klickbart och då leda till detaljsidan (sidorna som börjar med ordet Crime).
    
    JOBBAR PÅ ATT VALIDERA DATAT OCH ATT LÄNKARNA TILL ÄRENDENUMMER FUNGERAR SOM TÄNKT.

--- Miljoboven/Controllers/CoordinatorController.cs
+++ Miljoboven/Controllers/CoordinatorController.cs
@@ -1,20 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using Miljoboven.Models;
+using System.Collections.Generic;
 
 namespace Miljoboven.Controllers
 {
     public class CoordinatorController : Controller
     {
+
+        // Inject the repository
+        private readonly IMiljobovenRepository _repository;
+
+        public CoordinatorController(IMiljobovenRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // Fetching errands for Investigator view
+        public ViewResult StartCoordinator()
+        {
+            var errands = _repository.GetErrands();
+            return View(errands);
+        }
+
         public ViewResult CrimeCoordinator()
         {
             return View();
         }
 
         public ViewResult ReportCrime()
-        {
-            return View();
-        }
-
-        public ViewResult StartCoordinator()
         {
             return View();
         }
--- Miljoboven/Models/ErrandStatus.cs
+++ Miljoboven/Models/ErrandStatus.cs
@@ -1,10 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Miljoboven.Models;
 
 public class ErrandStatus
 {
-    [Key] public string StatusId { get; set; }
+    public string StatusId { get; set; }
 
     public string StatusName { get; set; }
 }
